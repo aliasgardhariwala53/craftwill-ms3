fb5ce79b208559bcd928dc17d7ce4da43e61e9b9
ali asgar <aliasgardhariwala53@gmail.com>
2022-02-24 13:33:36 +0530

    routing change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,34 +8,34 @@ const routes: Routes = [
     path : "",
     loadChildren : ()=>import("./modules/auth/auth.module").then(m1=>m1.AuthModule)
   },
-  {
-    path : "members",
-    loadChildren : ()=>import("./modules/members/members.module").then(m2=>m2.MembersModule)
-  },
-  {
-    path : "assets",
-    loadChildren : ()=>import("./modules/assets/assets.module").then(m2=>m2.AssetsModule)
-  },
+  // {
+  //   path : "members",
+  //   loadChildren : ()=>import("./modules/members/members.module").then(m2=>m2.MembersModule)
+  // },
+  // {
+  //   path : "assets",
+  //   loadChildren : ()=>import("./modules/assets/assets.module").then(m2=>m2.AssetsModule)
+  // },
   
-  {
-    path : "liabilities",
-    loadChildren : ()=>import("./modules/liabilities/liabilities.module").then(m2=>m2.LiabilitiesModule)
-  },
-  {
-    path : "will",
-    loadChildren : ()=>import("./modules/will/will.module").then(m2=>m2.WillModule)
-  },
-  {
-    path : "trust",
-    loadChildren : ()=>import("./modules/trust/trust.module").then(m2=>m2.TrustModule)
-  },
+  // {
+  //   path : "liabilities",
+  //   loadChildren : ()=>import("./modules/liabilities/liabilities.module").then(m2=>m2.LiabilitiesModule)
+  // },
+  // {
+  //   path : "will",
+  //   loadChildren : ()=>import("./modules/will/will.module").then(m2=>m2.WillModule)
+  // },
+  // {
+  //   path : "trust",
+  //   loadChildren : ()=>import("./modules/trust/trust.module").then(m2=>m2.TrustModule)
+  // },
   {
     path : "home",
     loadChildren : ()=>import("./modules/home/home.module").then(m2=>m2.HomeModule)
   },
   {
     path:'**',
-    redirectTo:'login',
+    redirectTo:'home',
   }
 ];
 @NgModule({
